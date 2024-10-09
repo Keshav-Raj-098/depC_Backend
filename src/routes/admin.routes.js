eedf0b8b20280e22047e6b281989743351be34a1
Keshav-Raj-098 <keshavraj09898@gmail.com>
2024-10-09 21:58:07 +0530

    Algo Added

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  {createBranchDetails,updateBranchDetails,deleteBranchDetails,getAllBranchDetails,getAllApplicants} from "../controllers/branchDetails.controller.js"
+import  {createBranchDetails,updateBranchDetails,deleteBranchDetails,getAllBranchDetails,getAllApplicants, ChangeBranch, removeChanges} from "../controllers/branchDetails.controller.js"
 
 const router = Router();
 
@@ -8,4 +8,9 @@ router.route("/update/:id").patch(updateBranchDetails)
 router.route("/delete/:id").delete(deleteBranchDetails)
 router.route("/readall").get(getAllBranchDetails)
 router.route("/readallApplicants").get(getAllApplicants)
+
+
+
+router.route("/applyChange").get(ChangeBranch)
+router.route("/removeChange").get(removeChanges)
 export default router
